aecfc85130da12d9d0ca16c656197db6c1ed68ef
Byte maya <bytemaya@gmail.com>
2022-03-18 22:37:15 +0530

    Initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Background from '../components/Background';
+
+import '../styles/index.css';
+
+export default function Home() {
+  return (
+    <div className='main-container'>
+      <Background />
+    </div>
+  );
+}
