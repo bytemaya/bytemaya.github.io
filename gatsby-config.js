e9e302b264c014b07759bc89b492ed0134c030e7
Byte maya <bytemaya@gmail.com>
2022-03-19 16:01:00 +0530

    Customized

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,80 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.com/docs/gatsby-config/
- */
+require(`dotenv`).config()
+
+const shouldAnalyseBundle = process.env.ANALYSE_BUNDLE
 
 module.exports = {
-  /* Your site config here */
-  plugins: [],
+  siteMetadata: {
+    // You can overwrite values here that are used for the SEO component
+    // You can also add new values here to query them like usual
+    // See all options: https://github.com/LekoArts/gatsby-themes/blob/main/themes/gatsby-theme-emilia-core/gatsby-config.js
+    siteTitle: `Byte maya`,
+    siteTitleAlt: `Byte maya`,
+    siteHeadline: `Byte maya`,
+    siteUrl: `http://bytemaya.com`,
+    siteDescription: `Home page of Byte maya.`,
+    siteLanguage: `en`,
+    siteImage: `/banner.jpg`,
+    author: `@byte_maya`,
+  },
+  plugins: [
+    {
+      resolve: `@lekoarts/gatsby-theme-emilia`,
+      // See the theme's README for all available options
+      options: {
+        name: `Byte maya`,
+        location: `Art admirer and Creative developer.`,
+        showThemeAuthor: false,
+        socialMedia: [
+          {
+            title: `Twitter`,
+            href: `https://twitter.com/byte_maya`
+          },
+          {
+            title: `Instagram`,
+            href: `https://www.instagram.com/bytemaya`
+          },
+          {
+            title: `FxHash`,
+            href: `https://www.fxhash.xyz/u/Byte%20maya`
+          }
+        ]
+      },
+    },
+    `gatsby-plugin-sitemap`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Emilia - @lekoarts/gatsby-theme-emilia`,
+        short_name: `Emilia`,
+        description: `Minimalistic portfolio/photography site with masonry grid, page transitions and big images. Themeable with Theme UI.`,
+        start_url: `/`,
+        background_color: `#fff`,
+        // This will impact how browsers show your PWA/website
+        // https://css-tricks.com/meta-theme-color-and-trickery/
+        // theme_color: `#3182ce`,
+        display: `standalone`,
+        icons: [
+          {
+            src: `/android-chrome-192x192.png`,
+            sizes: `192x192`,
+            type: `image/png`,
+          },
+          {
+            src: `/android-chrome-512x512.png`,
+            sizes: `512x512`,
+            type: `image/png`,
+          },
+        ],
+      },
+    },
+    `gatsby-plugin-gatsby-cloud`,
+    shouldAnalyseBundle && {
+      resolve: `gatsby-plugin-webpack-bundle-analyser-v2`,
+      options: {
+        analyzerMode: `static`,
+        reportFilename: `_bundle.html`,
+        openAnalyzer: false,
+      },
+    },
+  ].filter(Boolean),
 }
---
+++ src/@lekoarts/gatsby-theme-emilia/assets/location.tsx
@@ -0,0 +1,5 @@
+import * as React from "react"
+
+const Location = () => null
+
+export default Location
